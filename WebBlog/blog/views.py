2e22e6cdeadc8af25440fc712fb57bdb5f5e1986
Altria Vin <altriavin@foxmail.com>
2021-07-24 16:31:43 +0800

    add the html of login by Altria Vin in 2021.07.24

--- WebBlog/blog/views.py
+++ WebBlog/blog/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+import random
+from django.http import JsonResponse
 
-# Create your views here.
+from blog.models import *
+from django.forms.models import model_to_dict
+from django.core.mail import send_mail
+import requests
+import json
+import pandas as pd
+import os
+
+
+def test(request):
+    return JsonResponse({"data": 11111})
+
+
+def login(request):
+    if request.method == "GET":
+        username = request.GET.get("username")
+        password = request.GET.get("password")
+        print(username, password)
+    elif request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        print(username, password)
+    return render(request, "login.html")
